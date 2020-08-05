8b580fea08c2cd520a11655569f48e335342989d
Nga Vu <svu2711@gmail.com>
2020-08-06 03:53:42 +1000

    Updates

---
+++ precache-manifest.cd681e0060954d14cb8b3d7c7ca49aee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703dbb5f0a6d400ba6ffa77a3c3c1fbc",
+    "url": "/slickgoose/index.html"
+  },
+  {
+    "revision": "bb79582eaaaadc6ef7bc",
+    "url": "/slickgoose/static/css/main.fa528afc.chunk.css"
+  },
+  {
+    "revision": "69183ad451fe0180fdf1",
+    "url": "/slickgoose/static/js/2.abbb8488.chunk.js"
+  },
+  {
+    "revision": "bb79582eaaaadc6ef7bc",
+    "url": "/slickgoose/static/js/main.de7fcec3.chunk.js"
+  },
+  {
+    "revision": "b2db0635bd3a68535d95",
+    "url": "/slickgoose/static/js/runtime-main.b9d5e9f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30aa2144744718ae52ceff9ed6022477.js"
+  "/slickgoose/precache-manifest.cd681e0060954d14cb8b3d7c7ca49aee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/slickgoose/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
