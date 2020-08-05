7ae633893de94e74fba76033cd7291d09fd119ec
Nga Vu <svu2711@gmail.com>
2020-08-06 03:49:35 +1000

    Updates

---
+++ precache-manifest.30aa2144744718ae52ceff9ed6022477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd6d5b7717157ae29bd851bdb7d7f88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cb0a79c17e06911be25",
+    "url": "/static/css/main.fa528afc.chunk.css"
+  },
+  {
+    "revision": "41837e8d5691f0371d82",
+    "url": "/static/js/2.7ab9bc14.chunk.js"
+  },
+  {
+    "revision": "0cb0a79c17e06911be25",
+    "url": "/static/js/main.f1831e53.chunk.js"
+  },
+  {
+    "revision": "008c6e94dffad34358df",
+    "url": "/static/js/runtime-main.2fdac378.js"
+  }
+]);
